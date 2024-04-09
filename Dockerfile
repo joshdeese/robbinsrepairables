f2e87e9660dda322e10a7109bdf5e20987816dbb
Josh Deese <joshmdeese@gmail.com>
2024-04-09 09:40:59 -0400

    changed from puppeteer to node-html-parser and the native nodejs https

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,8 @@
 FROM node:20.11.1
-
-# Install the latest Chrome dev package and necessary fonts and libraries
-RUN apt-get update \
-  && apt-get install -y chromium\
-  fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
-  --no-install-recommends
-
-# Switch to the non-root user
 USER node
 RUN mkdir /home/node/app
-
-# Set the working directory
 WORKDIR /home/node/app
-
-# Copy package.json and package-lock.json
 COPY --chown=node package*.json ./
-
-# Update the PUPPETEER_EXECUTABLE_PATH to the correct Chrome path (placeholder, update based on the output of `which google-chrome-stable`)
-ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
-ENV PUPPETEER_EXECUTABLE_PATH /usr/bin/chromium
-
-# Install Puppeteer without downloading bundled Chromium
 RUN npm install
-
-# Copy your Puppeteer script into the Docker image
 COPY --chown=node . /home/node/app
-
-# Set the command to run your Puppeteer script
 CMD ["node", "main.js"]
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import puppeteer from 'puppeteer';
+import { parse } from 'node-html-parser';
+import https from 'https';
 import fs from 'fs/promises';
 import nodemailer from 'nodemailer';
 import mailgunTransport from 'nodemailer-mailgun-transport';
@@ -16,80 +17,90 @@ async function main() {
 
   const prev_ids = prev_data.map(a => { return a.vid } );
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(0);
-  const response = await page.goto('https://robbinsrepairables.com/vehicles.php');
+  https.get('https://robbinsrepairables.com/vehicles.php', res => {
+    let data = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
 
-  const cars = await page.$$eval('h3 a', (nodes) => {
-    return nodes.map(a => {
-      let myNode = a.parentNode.parentNode.parentNode;
-      let myImg = myNode.querySelector('img');
-      myImg.setAttribute('src', myImg.getAttribute('data-original'));
-      let hrefs = myNode.querySelectorAll('a');
-
-      for(var i=0; i<hrefs.length; i++){
-        hrefs[i].setAttribute('href', 'https://robbinsrepairables.com' + hrefs[i].getAttribute('href'));
-      }
-
-      return {
-        title: a.innerHTML,
-        link: a.href,
-        vid: a.href.split('=')[1],
-        block: myNode.outerHTML
-      }
+    res.on('data', chunk => {
+      data.push(chunk);
     });
-  });
 
-  let new_cars = cars.filter(a => { return prev_ids.indexOf(a.vid) == -1 });
-  const transport_opt = {
-    auth: {
-      domain: process.env.MAILGUN_DOMAIN,
-      api_key: process.env.MAILGUN_KEY
-    }
-  };
-  var transport = mailgunTransport(transport_opt); 
-  var transporter = nodemailer.createTransport(transport);
+    res.on('end', () => {
+      let body = parse(Buffer.concat(data).toString());
+      let nodes = body.querySelectorAll('h3 a');
+      let cars = nodes.map(a => {
+        let myNode = a.parentNode.parentNode.parentNode;
+        let myImg = myNode.querySelector('img');
+        myImg.setAttribute('src', myImg.getAttribute('data-original'));
+        let hrefs = myNode.querySelectorAll('a');
 
-  if(new_cars.length > 0){
-    let html = "";
-    for(var i=0; i<new_cars.length; i++){
-      html += '<hr>' + new_cars[i].block;
-    }
+        for(var i=0; i<hrefs.length; i++){
+          hrefs[i].setAttribute('href', 'https://robbinsrepairables.com' + hrefs[i].getAttribute('href'));
+        }
 
-    const mailOpts = {
-      from: process.env.MAIL_FROM,
-      to: process.env.MAIL_TO,
-      subject: "New Cars at Robbin's Repairables",
-      html: html
-    };
-    //console.log('mailOpts: ', mailOpts);
-    transporter.sendMail(mailOpts, (err, response) => {
-      if(err){
-        console.log('error sending email', err);
+        return {
+          title: a.innerHTML,
+          link: a.getAttribute('href'),
+          vid: a.getAttribute('href').split('=')[1],
+          block: myNode.outerHTML
+        }
+      });
+      let new_cars = cars.filter(a => { return prev_ids.indexOf(a.vid) == -1 });
+      const transport_opt = {
+        auth: {
+          domain: process.env.MAILGUN_DOMAIN,
+          api_key: process.env.MAILGUN_KEY
+        }
+      }
+      var transport = mailgunTransport(transport_opt); 
+      var transporter = nodemailer.createTransport(transport);
+
+      if(new_cars.length > 0){
+        let html = "";
+        for(var i=0; i<new_cars.length; i++){
+          html += '<hr>' + new_cars[i].block;
+        }
+
+        const mailOpts = {
+          from: process.env.MAIL_FROM,
+          to: process.env.MAIL_TO,
+          subject: "New Cars at Robbin's Repairables",
+          html: html
+        };
+        transporter.sendMail(mailOpts, (err, response) => {
+          if(err){
+            console.log('error sending email', err);
+          } else {
+            console.log('message sent');
+          }
+        });
       } else {
-        console.log('message sent');
+        const mailOpts = {
+          from: process.env.MAIL_FROM,
+          to: process.env.MAIL_TO,
+          subject: "New Cars at Robbin's Repairables",
+          text: "No new cars at this time"
+        };
+        console.log('no new cars at this time');
+        transporter.sendMail(mailOpts, (err, response) => {
+          if(err){
+            console.log('error sending email', err);
+          } else {
+            console.log('no new cars message sent');
+          }
+        });
       }
+
+      fs.writeFile('./data/data.json', JSON.stringify(cars), function(){});
     });
-    //console.log('new cars: ', new_cars );
-  } else {
-    const mailOpts = {
-      from: process.env.MAIL_FROM,
-      to: process.env.MAIL_TO,
-      subject: "New Cars at Robbin's Repairables",
-      text: "No new cars at this time"
-    };
-    console.log('no new cars at this time');
-  }
-
-  await fs.writeFile('./data/data.json', JSON.stringify(cars), function(){});
-
-  await browser.close();
+  }).on('error', err => {
+    console.log('Error: ', err.message);
+  });
 }
 
 const job = new CronJob(
-  //'0 0 8-20 * * *',
-  '*/10 * * * * *',
+  '0 0 8-20 * * *',
+  //'*/10 * * * * *',
   main,
   null,
   true,
